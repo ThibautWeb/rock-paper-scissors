11d8aeb06e62a250f527cf5efbf8b00fa5270a1c
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-07-21 22:59:39 +0200

    Implement game rules and scoring
    Input validation and game loop yet to come.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let computerScore = 0
 let humanScore = 0
 
-let options = ["rock","paper","scissors"]
+let options = ["rock", "paper", "scissors"]
 
 function getComputerChoice(){
   return options[Math.floor(Math.random() * 3)]
@@ -11,5 +11,23 @@ function getHumanChoice(){
   return prompt("Enter rock, paper, or scissors:")
 }
 
-console.log("Human choses " + getHumanChoice())
-console.log("Computer choses " + getComputerChoice())
+// console.log("Human choses " + getHumanChoice())
+// console.log("Computer choses " + getComputerChoice())
+
+function playRound(humanChoice, computerChoice){
+  if (humanChoice == computerChoice) {
+    console.log(`It's a draw! ${humanChoice} against ${computerChoice}`)
+  } else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+  (humanChoice == "paper" && computerChoice == "rock") ||
+  (humanChoice == "scissors" && computerChoice == "paper")) {
+    console.log(`Human wins! ${humanChoice} beats ${computerChoice}`)
+    humanScore++
+  } else {
+    console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`)
+    computerScore++
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice())
+console.log(`Human: ${humanScore}`)
+console.log(`Computer: ${computerScore}`)

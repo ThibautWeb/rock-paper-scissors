f7e58642ac71bdf9c1f647804d3addea37718ea7
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-07-22 08:11:41 +0200

    Wrap everything JS in playGame() function
    Because why not.

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-let options = ["rock", "paper", "scissors"]
-
-function getComputerChoice(){
-  return options[Math.floor(Math.random() * 3)]
-}
-
-function getHumanChoice(){
-  return prompt("Enter rock, paper, or scissors:")
-}
-
-// console.log("Human choses " + getHumanChoice())
-// console.log("Computer choses " + getComputerChoice())
-
-
 function playGame(rounds) {
+  let options = ["rock", "paper", "scissors"]
+
+  function getComputerChoice(){
+    return options[Math.floor(Math.random() * 3)]
+  }
+
+  function getHumanChoice(){
+    return prompt("Enter rock, paper, or scissors:")
+  }
   let computerScore = 0
   let humanScore = 0
 
